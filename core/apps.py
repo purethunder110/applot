6d33d97a8eb96b01fc8ee371943b6cd9a2005013
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2025-04-06 16:31:21 +0530

    fixing telegram class not visable

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 from django.apps import AppConfig
 
@@ -15,10 +14,11 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         # prevent twice run
-        run_once = os.environ.get("CMDLINERUNNER_RUN_ONCE")
-        if run_once is not None:
-            return
-        os.environ["CMDLINERUNNER_RUN_ONCE"] = "True"
+
+        # run_once = os.environ.get("CMDLINERUNNER_RUN_ONCE")
+        # if run_once is not None:
+        #     return
+        # os.environ["CMDLINERUNNER_RUN_ONCE"] = "True"
 
         # set telegram webhook
         self.telegram_bot = TelegramBot()
